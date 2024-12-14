009d8014647018ac8e7d8e4f3c3406024fd7249d
Ramya0301 <161931568+Ramya0301@users.noreply.github.com>
2024-12-14 16:02:17 +1100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,6 @@ const PORT = process.env.PORT || 1000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1",contact);
-app.listen(PORT,()=>{
-    console.log(`server listen at port ${PORT}`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server listening on port 3000');
 })
